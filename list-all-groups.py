d46a2ddde10961326b38a19143365255b937e9e0
Jason Price <jason.price@weather.com>
2015-07-22 10:05:09 -0400

    list-all-groups.py does not require group on the cli

--- list-all-groups.py
+++ list-all-groups.py
@@ -13,15 +13,13 @@ def parse_cli():
     """duh."""
     parser = argparse.ArgumentParser(description='update netscaler group definition')
     parser = netscaler.parse_cli_netscaler_config(parser)
-    parser = netscaler.parse_cli_group_config(parser)
     args = parser.parse_args()
     nsconf = netscaler.validate_cli_netscaler_config(args)
-    groupconf = netscaler.validate_cli_group_config(args)
-    return args, nsconf, groupconf
+    return args, nsconf
 
 
 if __name__ == '__main__':
-    args, nsconf, groupconf = parse_cli()
+    args, nsconf = parse_cli()
     s = requests.session()
-    results = netscaler.list_groups(s, nsconf, groupconf)
+    results = netscaler.list_groups(s, nsconf)
     pprint.pprint(results)
